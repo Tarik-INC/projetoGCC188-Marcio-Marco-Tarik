0cdedd9fc364a40e39ae91170662a73f3abc2266
Marco <maureliocfh@gmail.com>
2018-04-25 18:13:34 -0300

    incluindo funcionalidade RF03 e RF04

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,23 @@ def incremento(x):
 
 print(incremento(4))
 
+x = input("Primeiro Numero: ")
+y = input("Segundo Numero: ")
+
+def add (x, y):
+	soma = int(x) + int(y)
+	return int(soma)
+
+soma = add(x,y)
+print(soma)
+
+x = input("Primeiro Numero: ")
+y = input("Segundo Numero: ")
+
+def sub (x, y):
+	soma = int(x) - int(y)
+	return int(soma)
+
+sub = sub(x,y)
+print(sub)
+
